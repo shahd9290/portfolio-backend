af4d874191e0856abe7eb448d926ad042ac3b824
Danyal <shahd9290@gmail.com>
2025-09-18 20:31:27 +0100

    Endpoint Tests

---
+++ src/routes/backendRoutes.js
@@ -0,0 +1,21 @@
+const routes = (app) => {
+    app.route('/contact')
+        .get((req, res) => {
+            res.send("GET request");
+        })
+
+        .post((req, res) => {
+            res.send("POST request");
+        })
+
+    app.route('/contact/:contactId')
+        .put((req, res) => {
+            res.send("PUT request");
+        })
+
+        .delete((req, res) => {
+            res.send("DELETE request");
+        })
+}
+
+export default routes;
