3ba86038446f0519486374330e1d9eda13fb05a9
Danyal <shahd9290@gmail.com>
2025-09-19 18:24:28 +0100

    Configure .envs

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import routes from './src/routes/backendRoutes';
+import 'dotenv/config';
+
 
 const app = express();
-const PORT = 3000
+const PORT = process.env.PORT;
+const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@portfolio-backend.jqwkh3w.mongodb.net/?retryWrites=true&w=majority&appName=portfolio-backend`, {
+    useNewUrlParser: true,
+});
+
+// body parser setup
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 routes(app);
 
